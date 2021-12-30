fd9d523732f54df383b2f978712396942435e3cf
Sergey Goncharuk <fleshed914@gmail.com>
2021-12-30 03:38:19 +0300

    feat: add smooth scroll to promo__arrow

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,19 @@ new Accordion(['.accordion-container'], {
   openOnInit: [2],
   collapse: false
 });
+
+
+
+
+
+$( document ).ready(function() {
+  $('a[href^="#"').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+  });
+});
